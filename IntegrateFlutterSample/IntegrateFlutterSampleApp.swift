bef577fa9c8917d9a093aaa3fe150277461eb47c
mduccc <mducc1412@gmail.com>
2023-04-21 01:27:45 +0700

    Initial Commit

---
+++ IntegrateFlutterSample/IntegrateFlutterSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntegrateFlutterSampleApp.swift
+//  IntegrateFlutterSample
+//
+//  Created by Đức Minh on 21/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct IntegrateFlutterSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
